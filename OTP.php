e7022dbd4b033afe392a3d41f12aec81a7accc60
iTaha <62572903+itstaha@users.noreply.github.com>
2020-05-21 14:18:01 +0430

    Create OTP.php

---
+++ OTP.php
@@ -0,0 +1,34 @@
+<?php
+class OTP {
+    private function unique() {
+        $unique = uniqid();
+        $unique = preg_replace('/([a-zA-Z])/','', $unique);
+        $this->unique_id = $unique;
+    }
+    private function shuffle($str) {
+        $shuffle = str_shuffle($str);
+        return $shuffle;
+    }
+    private function mix($items,$n) {
+        $num = 0;
+        foreach($items as $item) {
+            foreach($item as $number) {
+                $num .= $number;
+            }
+        }
+        $shuffle = $this->shuffle($num);
+        return substr($shuffle, 0, $n);
+    }
+    public function genrate($n) {
+        $list = [];
+        $unique_id = $this->unique();
+        for($i=1;$i<=$n;$i++) {
+            $first = $this->shuffle($unique_id);
+            $second = $this->shuffle(time());
+            $arr = [$first,$second];
+            array_push($list,$arr);
+        }
+        $mix = $this->mix($list,$n);
+        return $mix;
+    }
+}
